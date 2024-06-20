bf92ceb83b4fd2aefa5fe02e71ce5b2c0bfa95f2
Enzo <kurutsukipv@gmail.com>
2024-06-20 18:12:32 +0200

    Add files via upload

---
+++ ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emagnani <emagnani@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/20 14:13:04 by emagnani          #+#    #+#             */
+/*   Updated: 2024/06/20 14:13:05 by emagnani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIBFT_H
+# define FT_LIBFT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <stddef.h>
+
+#endif
